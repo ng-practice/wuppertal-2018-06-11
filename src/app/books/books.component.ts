d9df5327f04110ce4768c7d9416960cd35c7d4a7
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-12 09:58:24 +0200

    refactor(books): use onInit hook

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -8,16 +8,16 @@ import { Book } from './models/book';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  books = this.booksService.getAll();
+  books: Book[];
 
   constructor(private booksService: BooksService) {}
+
+  ngOnInit() {
+    this.books = this.booksService.getAll();
+  }
 
   sortBooks(book: Book) {
     this.books.sort((current, next) => next.rating - current.rating);
     console.log(book);
   }
-
-  ngOnInit() {
-  }
-
 }
