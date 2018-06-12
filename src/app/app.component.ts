547ba65c596031f9bb343226f8d0aee9fe5ac40a
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-12 09:31:39 +0200

    service: introduce IoC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Book } from './books/models/book';
+import { BooksService } from './books/lib/books.service';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +11,9 @@ export class AppComponent {
   title = 'Workshop in Wuppertal';
   subtitle = 'Foundation Level';
 
-  books = [
-    new Book(
-      '213-123-3123-43',
-      'Angular 6',
-      'One Framework for Web and Mobile',
-      ['Misko Hervery', 'Igor Minar'],
-      5
-    ),
-    new Book(
-      '533-123-3123-41',
-      'NodeJS',
-      'Server Side JavaScript',
-      ['Ryan Dahl'],
-      4
-    )
-  ];
+  books = this.booksService.getAll();
+
+  constructor(private booksService: BooksService) {}
 
   sortBooks(book: Book) {
     this.books.sort((current, next) => next.rating - current.rating);
---
+++ src/app/books/lib/books.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { BooksModule } from '../books.module';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: BooksModule
+})
+export class BooksService {
+  getAll(): Book[] {
+    return [
+      new Book(
+        '213-123-3123-43',
+        'Angular 6',
+        'One Framework for Web and Mobile',
+        ['Misko Hervery', 'Igor Minar'],
+        5
+      ),
+      new Book(
+        '533-123-3123-41',
+        'NodeJS',
+        'Server Side JavaScript',
+        ['Ryan Dahl'],
+        4
+      )
+    ];
+  }
+}
