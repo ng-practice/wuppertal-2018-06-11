3c1332315be0ecc6c2d35eb44c12685dc1dce3cb
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-13 11:50:49 +0200

    provide not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
 import { BooksComponent } from './books/books.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Route[] = [
   { path: '', redirectTo: '/books', pathMatch: 'full' },
-  { path: 'books', component: BooksComponent }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
